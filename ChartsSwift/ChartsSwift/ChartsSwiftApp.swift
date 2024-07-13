3d89a2e342a16078d6ad2c403dee48b6067bfea5
arpit verma <arpitverma@arpits-Mac.local>
2024-07-13 19:53:02 +0530

    Day 4 Charts

---
+++ ChartsSwift/ChartsSwift/ChartsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSwiftApp.swift
+//  ChartsSwift
+//
+//  Created by arpit verma on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
