cf563f759d8bd247612162a0b893a8560001cf59
arpitvermaofficial <arpitv747@gmail.com>
2024-07-20 00:05:13 +0530

    Day 10 DatePicker

---
+++ DatePicker/DatePicker/DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerApp.swift
+//  DatePicker
+//
+//  Created by arpit verma on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
