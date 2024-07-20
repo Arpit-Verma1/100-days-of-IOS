50c3c1ad66a09b82edf3fcec2b2440175c5f0c97
arpitvermaofficial <arpitv747@gmail.com>
2024-07-20 19:54:22 +0530

    Day11_image_Download

---
+++ ImageDownload/ImageDownload/ImageDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloadApp.swift
+//  ImageDownload
+//
+//  Created by arpit verma on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
