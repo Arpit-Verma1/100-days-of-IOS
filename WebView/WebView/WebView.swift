5f67c726850bc62181823a64ca5e3b744c62c337
arpitvermaofficial <arpitv747@gmail.com>
2024-08-02 23:51:57 +0530

    Day23_WebVew

---
+++ WebView/WebView/WebView.swift
@@ -0,0 +1,12 @@
+//
+//  WebView.swift
+//  WebView
+//
+//  Created by arpit verma on 02/08/24.
+//
+
+import Foundation
+import   SwiftUI
+import   WebKit
+
+struct SwiftUIWebView:
---
+++ WebView/WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by arpit verma on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
