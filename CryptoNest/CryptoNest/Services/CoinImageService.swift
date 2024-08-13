453d05446dea1e44b2b23f62e4b2908c2d9e6966
arpitvermaofficial <arpitv747@gmail.com>
2024-08-13 14:21:40 +0530

    Day31_CryptoNest: Save coin image to fileManegaer

--- CryptoNest/CryptoNest/Services/CoinImageService.swift
+++ CryptoNest/CryptoNest/Services/CoinImageService.swift
@@ -11,15 +11,31 @@ import Combine
 class CoinImageService {
     @Published var image : UIImage? = nil
     private let coin: CoinModel
-    
-    
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coinImages"
+    private let imageName : String
     private var imageSubscription : AnyCancellable?
     
     init(coin:CoinModel) {
         self.coin = coin
-        getCoinImage()
+        self.imageName = coin.id
+        getCoinImage()	
     }
+    
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+           image = savedImage
+            print("get image from folder")
+        }
+        else {
+            downloadCoinImage()
+            print("downloading image")
+        }
+            
+        
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string:  coin.image) else { return }
        
         imageSubscription = NetworkingManager.download(url: url)
@@ -27,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self,  let downloadImage = returnedImage else {return }
+                self.image = downloadImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadImage, imageName: self.imageName, folderName: self.folderName)
             })
            
             
